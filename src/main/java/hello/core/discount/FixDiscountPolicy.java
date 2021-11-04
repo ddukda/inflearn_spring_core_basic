554de8a5a780767f087e198a37fc1fc35afd5589
jino <ddukda@nhnsoft.com>
2021-11-05 08:31:11 +0900

    first

---
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -0,0 +1,18 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+public class FixDiscountPolicy implements DiscountPolicy {
+
+    private int discoutFixAmount = 1000;
+
+    @Override
+    public int discount(Member member, int price) {
+        if(member.getGrade().equals(Grade.VIP)) {
+            return discoutFixAmount;
+        } else {
+            return 0;
+        }
+    }
+}
