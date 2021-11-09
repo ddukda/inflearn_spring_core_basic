03f380d560fdfa3f252586d84a46824253a2c48b
jino <ddukda@nhnsoft.com>
2021-11-09 10:51:12 +0900

    스프링 컨테이너, 스프링 빈

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -5,13 +5,26 @@ import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
 
+    /**
+     * ApplicationContext > 스프링 컨테이너
+     * 스프링컨테이너는 @Configuration 이 붙은 AppConfig 를 구성정보로 사용
+     * @Bean 이라고 적힌 메서드를 모두 호출해서 반환된 객체를 스프링 컨테이너에 등록, 이것을 스프링 빈이라고 함
+     * 스프링 컨테이너를 통해 통해 스프링 빈을 찾아 사용
+     */
+
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
+
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        OrderService orderService = ac.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
