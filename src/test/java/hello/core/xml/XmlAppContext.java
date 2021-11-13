e9aca02e26c5522dd5247aa7f866396dbf294329
jino <ddukda@nhnsoft.com>
2021-11-13 23:13:47 +0900

    스프링빈 xml 설정

---
+++ src/test/java/hello/core/xml/XmlAppContext.java
@@ -0,0 +1,23 @@
+package hello.core.xml;
+
+import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import java.util.Map;
+
+public class XmlAppContext {
+
+    @Test
+    void xmlAppContext() {
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+        Map<String, MemberService> beansOfType = ac.getBeansOfType(MemberService.class);
+        for (String key : beansOfType.keySet()) {
+            System.out.println("key = " + key + ", value = " + beansOfType.get(key));
+        }
+    }
+}
