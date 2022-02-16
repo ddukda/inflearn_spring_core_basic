7eb262372ae33a61b0b52804f192761402b774ac
JINO Y <ddukda@gmail.com>
2022-02-16 09:27:48 +0900

    Revert "Merge pull request #1 from ddukda/master"
    This reverts commit fd692747326ce6cf037b17069e0e8ff02ae1183c.

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -10,5 +13,9 @@ import org.springframework.context.annotation.FilterType;
         excludeFilters= @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class) // AppConfig 제외, configuration 어노테이션에 component 존재
 )
 public class AutoAppConfig {
+
+
     // 자동빈이 있고 수동빈이 있으면, 수동빈이 우선, 자동빈을 오버라이딩 해버림)
+//     `
+
 }
